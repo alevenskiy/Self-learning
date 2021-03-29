d264b52cb5dc8edc5d9286393cfaaf3eb4d8dafc
alevenskiy <ppcrn97@gmail.com>
2021-03-29 16:54:10 +0300

    initial commit

---
+++ sum/Sum.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std; //удобсвто это важно
+
+int main()
+{
+    float a;
+    float b;
+    cout << "a + b = c \n";
+    
+    cout << "float a = ";
+    cin >> a;
+    
+    cout << "\nfloat b = ";
+    cin >> b;
+    
+    cout << "a + b = \n" << a + b;
+}
